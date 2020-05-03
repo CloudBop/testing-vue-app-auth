738d7bc2fa3a2fa0b2dd78069b1b2390750d626a
Cloudbop <boppincolin@gmail.com>
2020-05-03 17:27:26 +0100

    implemented test of login + validators

--- __tests__/server/validators/login.spec.js
+++ __tests__/server/validators/login.spec.js
@@ -5,6 +5,17 @@
  */
 import loginValidator from '@validators/login';
 //
+// mock Express response fn
+class Response {
+  status(status) {
+    this.status = status;
+    return this;
+  }
+  json(data) {
+    return data;
+  }
+}
+//
 describe('the login validator', () => {
   //
   it('should call the next function (middleware) when validation succeeds', async () => {
@@ -24,5 +35,31 @@ describe('the login validator', () => {
     await loginValidator(req, res, next);
     //
     expect(next).toHaveBeenCalled();
+  });
+  //
+  //
+  it('should return a 422 error if validation fails', async () => {
+    const req = {
+      body: {
+        password: 'password'
+      }
+    };
+    //
+    const next = jest.fn();
+
+    const res = new Response();
+
+    const statusSpy = jest.spyOn(res, 'status');
+    const jsonSpy = jest.spyOn(res, 'json');
+    await loginValidator(req, res, next);
+    // 422 error code
+    expect(statusSpy).toHaveBeenCalledWith(422);
+    // yup api
+    expect(jsonSpy).toHaveBeenCalled({
+      message: 'Validation failed.',
+      data: {
+        email: 'email is a required field'
+      }
+    });
   });
 });
