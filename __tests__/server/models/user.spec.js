ea6c6ea617d4a372a054e014ce092319f083432d
Cloudbop <boppincolin@gmail.com>
2020-05-03 13:37:33 +0100

    setup jest and created first test

---
+++ __tests__/server/models/user.spec.js
@@ -0,0 +1,30 @@
+/**
+ * 
+ * @jest-environment node
+ * 
+ */
+import User from '@models/User';
+//
+import mongoose from 'mongoose';
+//
+import Bcrypt from 'bcryptjs';
+//
+describe('The User model', () => {
+  it('should hash the user password before saving to the db', async () => {
+    // connect to db
+    await mongoose.connect('mongodb://localhost:27017/auth-app_test', { useNewUrlParser: true });
+    // create new test user
+    const user = {
+      name: 'Test User',
+      email: 'test@user.com',
+      password: 'password'
+    };
+    //
+    // create new user from User Model
+    const createdUser = await User.create(user);
+    // comparse sync compares unhashed string with hashed string
+    expect(Bcrypt.compareSync(user.password, createdUser.password)).toBe(true);
+    //
+  });
+});
+// by default jest is setup for jsdom. The comment at top of file tell jest to test for node env.
